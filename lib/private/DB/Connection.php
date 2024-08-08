lwenderoth/server

feat: add option to automatically partition queries by specific tables
Signed-off-by: Robin Appelman <robin@icewind.nl>

lib/private/DB/Connection.php
@@ -23,6 +23,8 @@ use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Statement;
+use OC\DB\QueryBuilder\Partitioned\PartitionDefinition;
+use OC\DB\QueryBuilder\Partitioned\PartitionedQueryBuilder;
 use OC\DB\QueryBuilder\QueryBuilder;
 use OC\SystemConfig;
 use OCP\DB\QueryBuilder\IQueryBuilder;
@@ -75,6 +77,9 @@ class Connection extends PrimaryReadReplicaConnection {
 	protected bool $logRequestId;
 	protected string $requestId;
 
+	/** @var array<string, list<string>> */
+	protected array $partitions;
+
 	/**
 	 * Initializes a new instance of the Connection class.
 	 *
@@ -116,6 +121,8 @@ class Connection extends PrimaryReadReplicaConnection {
 			$this->dbDataCollector->setDebugStack($debugStack);
 			$this->_config->setSQLLogger($debugStack);
 		}
+
+		$this->partitions = $this->systemConfig->getValue('db.partitions', []);
 
 		$this->setNestTransactionsWithSavepoints(true);
 	}
@@ -168,11 +175,24 @@ class Connection extends PrimaryReadReplicaConnection {
 	 */
 	public function getQueryBuilder(): IQueryBuilder {
 		$this->queriesBuilt++;
-		return new QueryBuilder(
-			new ConnectionAdapter($this),
-			$this->systemConfig,
-			$this->logger
-		);
+		if (count($this->partitions) > 0) {
+			$builder = new PartitionedQueryBuilder(
+				new ConnectionAdapter($this),
+				$this->systemConfig,
+				$this->logger
+			);
+			foreach ($this->partitions as $name => $tables) {
+				$partition = new PartitionDefinition($name, $tables);
+				$builder->addPartition($partition);
+			}
+			return $builder;
+		} else {
+			return new QueryBuilder(
+				new ConnectionAdapter($this),
+				$this->systemConfig,
+				$this->logger
+			);
+		}
 	}
 
 	/**

cloudbank-pmb/nc-server

lib/private/DB/Connection.php
@@ -23,6 +23,8 @@ use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Statement;
+use OC\DB\QueryBuilder\Partitioned\PartitionSplit;
+use OC\DB\QueryBuilder\Partitioned\PartitionedQueryBuilder;
 use OC\DB\QueryBuilder\QueryBuilder;
 use OC\SystemConfig;
 use OCP\DB\QueryBuilder\IQueryBuilder;
@@ -75,6 +77,9 @@ class Connection extends PrimaryReadReplicaConnection {
 	protected bool $logRequestId;
 	protected string $requestId;
 
+	/** @var array<string, list<string>> */
+	protected array $partitions;
+
 	/**
 	 * Initializes a new instance of the Connection class.
 	 *
@@ -116,6 +121,8 @@ class Connection extends PrimaryReadReplicaConnection {
 			$this->dbDataCollector->setDebugStack($debugStack);
 			$this->_config->setSQLLogger($debugStack);
 		}
+
+		$this->partitions = $this->systemConfig->getValue('db.partitions', []);
 
 		$this->setNestTransactionsWithSavepoints(true);
 	}
@@ -168,11 +175,21 @@ class Connection extends PrimaryReadReplicaConnection {
 	 */
 	public function getQueryBuilder(): IQueryBuilder {
 		$this->queriesBuilt++;
-		return new QueryBuilder(
+		$builder = new QueryBuilder(
 			new ConnectionAdapter($this),
 			$this->systemConfig,
 			$this->logger
 		);
+		if (count($this->partitions) > 0) {
+			$builder = new PartitionedQueryBuilder($builder);
+			foreach ($this->partitions as $name => $tables) {
+				$partition = new PartitionSplit($name, $tables);
+				$builder->addPartition($partition);
+			}
+			return $builder;
+		} else {
+			return $builder;
+		}
 	}
 
 	/**