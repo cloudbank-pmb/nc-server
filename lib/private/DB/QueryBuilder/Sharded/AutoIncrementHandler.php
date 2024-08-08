80aad73bfa59f46efc25b4997ee86a2a407cc644
Robin Appelman <robin@icewind.nl>
2024-08-08 18:16:00 +0200

    fix: delay calculating global cache prefix untill a cache is created
    this makes it easier to break DI circles
    
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- lib/private/DB/QueryBuilder/Sharded/AutoIncrementHandler.php
+++ lib/private/DB/QueryBuilder/Sharded/AutoIncrementHandler.php
@@ -19,22 +19,27 @@ class AutoIncrementHandler {
 	const MIN_VALID_KEY = 1000;
 	const TTL = 365 * 24 * 60 * 60;
 
-	private IMemcache $cache;
+	private ?IMemcache $cache = null;
 
 	public function __construct(
-		ICacheFactory                  $cacheFactory,
+		private ICacheFactory                  $cacheFactory,
 		private ShardConnectionManager $shardConnectionManager,
 	) {
 		if (PHP_INT_SIZE < 8) {
 			throw new \Exception("sharding is only supported with 64bit php");
 		}
+	}
 
-		$cache = $cacheFactory->createDistributed("shared_autoincrement");
-		if ($cache instanceof IMemcache) {
-			$this->cache = $cache;
-		} else {
-			throw new \Exception('Distributed cache ' . get_class($cache) . ' is not suitable');
+	private function getCache(): IMemcache {
+		if(is_null($this->cache)) {
+			$cache = $this->cacheFactory->createDistributed("shared_autoincrement");
+			if ($cache instanceof IMemcache) {
+				$this->cache = $cache;
+			} else {
+				throw new \Exception('Distributed cache ' . get_class($this->cache) . ' is not suitable');
+			}
 		}
+		return $this->cache;
 	}
 
 	public function getNextPrimaryKey(ShardDefinition $shardDefinition): int {
@@ -64,11 +69,11 @@ class AutoIncrementHandler {
 		// care must be taken that the logic remains fully resilient against race conditions
 
 		// prevent inc from returning `1` if the key doesn't exist by setting it to a non-numeric value
-		$this->cache->add($shardDefinition->table, "empty-placeholder", self::TTL);
-		$next = $this->cache->inc($shardDefinition->table);
+		$this->getCache()->add($shardDefinition->table, "empty-placeholder", self::TTL);
+		$next = $this->getCache()->inc($shardDefinition->table);
 
 		if ($this->cache instanceof IMemcacheTTL) {
-			$this->cache->setTTL($shardDefinition->table, self::TTL);
+			$this->getCache()->setTTL($shardDefinition->table, self::TTL);
 		}
 
 		// the "add + inc" trick above isn't strictly atomic, so as a safety we reject any result that to small
@@ -77,7 +82,7 @@ class AutoIncrementHandler {
 			return $next;
 		} elseif (is_int($next)) {
 			// we hit the edge case, so invalidate the cached value
-			if (!$this->cache->cas($shardDefinition->table, $next, "empty-placeholder")) {
+			if (!$this->getCache()->cas($shardDefinition->table, $next, "empty-placeholder")) {
 				// someone else is changing the value concurrently, give up and retry
 				return null;
 			}
@@ -86,21 +91,21 @@ class AutoIncrementHandler {
 		// discard the encoded initial shard
 		$current = $this->getMaxFromDb($shardDefinition) >> 8;
 		$next = max($current, self::MIN_VALID_KEY) + 1;
-		if ($this->cache->cas($shardDefinition->table, "empty-placeholder", $next)) {
+		if ($this->getCache()->cas($shardDefinition->table, "empty-placeholder", $next)) {
 			return $next;
 		}
 
 		// another request set the cached value before us, so we should just be able to inc
-		$next = $this->cache->inc($shardDefinition->table);
+		$next = $this->getCache()->inc($shardDefinition->table);
 		if (is_int($next) && $next >= self::MIN_VALID_KEY) {
 			return $next;
 		} else if(is_int($next)) {
 			// key got cleared, invalidate and retry
-			$this->cache->cas($shardDefinition->table, $next, "empty-placeholder");
+			$this->getCache()->cas($shardDefinition->table, $next, "empty-placeholder");
 			return null;
 		} else {
 			// cleanup any non-numeric value other than the placeholder if that got stored somehow
-			$this->cache->ncad($shardDefinition->table, "empty-placeholder");
+			$this->getCache()->ncad($shardDefinition->table, "empty-placeholder");
 			// retry
 			return null;
 		}
